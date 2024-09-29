85aad597424391d56b72b061751ebe4d258440e8
vamsi <vamsibattu3015@gmail.com>
2024-09-29 09:17:51 +0530

    modified

--- 07-file-permission.sh
+++ 07-file-permission.sh
@@ -2,11 +2,11 @@
 
 file="01-Hello-World.sh"
 
-if [ -r "$file"];then
+if [ -r "$file"]; then
     echo "readable"
-elif [ -w "$file"];then
+elif [ -w "$file"]; then
     echo "writable"
-elif [ -x "$file"];then
+elif [ -x "$file"]; then
     echo "executable"
 else
     echo "No permission"
