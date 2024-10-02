e7838201d6d8534e7fc2c03f2d534bd7634bb771
vamsi <vamsibattu3015@gmail.com>
2024-10-02 18:52:27 +0530

    backup files

---
+++ 18-backup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source_dir=$1
+dest_dir=$2
+days=${3:-14}
+
+usage() {
+    echo " usage: sh 18-backup.sh <source> <destination> <days(optional)>"
+}
+
+if [ $# -lt 2 ]; then
+    usage
+fi
+
+if [ ! -d $source_dir ]; then
+    echo "$source_dir does not exist... please check"
+fi
+
+if [ ! -d $dest_dir ]; then
+    echo "$dest_dir does not exist... please check"
+fi
+
+files=$(find ${source_dir} -name "*.log" -mtime +14)
+
+if [ ! -z $files]; then
+    echo "files are found"
+else
+    echo "no files older than $days"
+fi
+
+
+ 
