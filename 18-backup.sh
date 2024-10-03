7681e57f3bd39730f93f7c0836345f1b1b37fc6f
vamsi <vamsibattu3015@gmail.com>
2024-10-03 11:47:22 +0530

    backup files

--- 18-backup.sh
+++ 18-backup.sh
@@ -3,6 +3,7 @@
 source_dir=$1
 dest_dir=$2
 days=${3:-14}
+timestamp=$( date +%Y-%m-%d-%H-%M-%S )
 
 usage() {
     echo " usage: sh 18-backup.sh <source> <destination> <days(optional)>"
@@ -13,8 +14,8 @@ if [ $# -lt 2 ]; then
 fi
 
 for dir in "$source_dir" "$dest_dir"; do
-    if [ ! -d $dir ]; then
-        echo " $dir does not exist...please check"
+    if [ ! -d "$dir" ]; then
+        echo "$dir does not exist...please check"
     fi
 done
 
@@ -22,10 +23,26 @@ files=$(find "${source_dir}" -name "*.log" -mtime +14)
 
 if [ -n "$files" ]; then
     echo "files are found"
+    zip_file="$dest_dir/app-logs-$timestamp.zip"
+    zip "$zip_file" $files
+    if [ -f "$zip_file" ]; then
+        echo "Files have been successfully zipped into $zip_file"
+        #remove the files after zipping
+        while IFS= read -r file
+        do
+            echo "deleting file: $file"
+            rm -rf $file
+        done <<< $files
 
+    else
+        echo "Failed to create the zip file"
+        exit 1
+    fi
 else
     echo "no files older than $days"
 fi
+
+
 
 
 
